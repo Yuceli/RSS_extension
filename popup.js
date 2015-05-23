e3a9189384f62eb7638e8dac729e2d0a23ae678e
bheftye <bheftye92@gmail.com>
2015-05-22 20:55:35 -0500

    Extension Funcional

--- popup.js
+++ popup.js
@@ -1,20 +1,37 @@
 function getRss(event){
-    var resultado = "";
+    /*var resultado = "";
     $.ajax({ 
             url: "http://brentheftye.mx/rss/index.php",
             type:'GET',
-            contentType:false,
-            processData:false,
-            cache:false,
-            async:false,
+            crossDomain: true,
+            dataType: 'jsonp',
             success:function(data){
                  if(data != ""){
                     resultado = JSON.parse(data);
+                    console.log(resultado);
+                    document.getElementById('elemento').innerHTML = resultado;
                 }
-                console.log(resultado);
-                document.getElementById('elemento').innerHTML = resultado;
             }
         });
+    */
+    var xmlhttp;
+    if (window.XMLHttpRequest)
+      {// code for IE7+, Firefox, Chrome, Opera, Safari
+      xmlhttp=new XMLHttpRequest();
+      }
+    else
+      {// code for IE6, IE5
+      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+      }
+    xmlhttp.onreadystatechange=function()
+      {
+      if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+        document.getElementById("elemento").innerHTML= JSON.parse(xmlhttp.responseText);
+        }
+      }
+    xmlhttp.open("GET","http://brentheftye.mx/rss/index.php",true);
+    xmlhttp.send();
 }
 
 $(document).ready(function($) {
